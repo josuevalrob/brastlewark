53dc9903e6da47aeb29f0af9f7ed6fb46477d3b0
Josue Valencia <josue.valencia@domo.ec>
2019-04-19 16:09:44 +0200

    implementing easy loading

--- src/components/listItem.js
+++ src/components/listItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import LazyLoad from 'react-lazyload';
+
 
 const ListItem = (props) => {
   return (  
     <div key={props.item.id}>
       <p>{props.item.name}</p> 
       <p>{props.item.age}</p>
+      <LazyLoad throttle={200} height={300}>
+        <img src={props.item.thumbnail} alt={props.item.name}/>
+      </LazyLoad>
     </div>
   )
 }
